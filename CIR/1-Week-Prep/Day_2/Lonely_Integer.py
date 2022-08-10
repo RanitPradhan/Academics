f6f868cfed9a86abb0393487fa27e604071bb400
V!p3r <56351938+RanitPradhan@users.noreply.github.com>
2022-08-10 17:00:03 +0530

    File Added

---
+++ CIR/1-Week-Prep/Day_2/Lonely_Integer.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+    # Write your code here
+    r=a[0]
+    for i in range(1,n):
+        r=r^a[i] # whren r!= a[i] the loop will break
+    return(r)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
