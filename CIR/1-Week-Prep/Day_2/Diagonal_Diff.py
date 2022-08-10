44cd2800afc6f181794166d59607eccaf688d2bd
V!p3r <56351938+RanitPradhan@users.noreply.github.com>
2022-08-10 17:05:24 +0530

    Added new file

---
+++ CIR/1-Week-Prep/Day_2/Diagonal_Diff.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    # Write your code here
+    sum1=0
+    sum2=0
+    for i in range (0,n):
+        for j in range(0,n):
+            if i==j: # for the primary diagonal
+                sum1+=arr[i][j]
+            if (i==n-j-1): # for the secondary diagonal
+                sum2+=arr[i][j]
+    return abs(sum1-sum2)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
