fb9268002091c8a1ed099bc1b9483cd8dc15a21f
V!p3r <56351938+RanitPradhan@users.noreply.github.com>
2022-09-11 18:10:55 +0530

    Another approach added

--- CIR/Regular/string-validation.py
+++ CIR/Regular/string-validation.py
@@ -4,3 +4,31 @@ print(any(map(str.isalpha,s)))
 print(any(map(str.isdigit,s)))
 print(any(map(str.islower,s)))
 print(any(map(str.isupper,s)))
+
+# Another method
+
+'''
+s = input()
+one = False
+two = False
+three = False
+four = False
+five = False
+
+for i in s:
+    if not one:
+        one = i.isalnum()
+    if not two:
+        two = i.isalpha()
+    if not three:
+        three = i.isdigit()
+    if not four:
+        four = i.islower()
+    if not five:
+        five = i.isupper()
+print(one)
+print(two)
+print(three)
+print(four)
+print(five)
+'''
