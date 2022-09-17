e1de53cb1d2d76793e191d33b53cb76d16d01a67
V!p3r <56351938+RanitPradhan@users.noreply.github.com>
2022-09-17 14:50:03 +0530

    DS-1 uploaded

---
+++ CIR/Regular/Arrays-DS.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'reverseArray' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def reverseArray(a):
+    # Write your code here
+    b=[]
+    l=len(a)
+    for i in range(l):
+        b.append(a[l-1])
+        l=l-1
+    return(b)
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    res = reverseArray(arr)
+
+    fptr.write(' '.join(map(str, res)))
+    fptr.write('\n')
+
+    fptr.close()
